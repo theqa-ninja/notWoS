9066e9387876fa1947dffd847a55b83196960a18
Amy Dang <kirontoo@gmail.com>
2022-03-21 19:24:30 -0700

    Configure how css is loaded

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,9 +4,9 @@
 */
 
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
   plugins: []
-}
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,4 @@ export default defineConfig({
     port: 9000
   },
   plugins: [react(), tsconfigPaths()]
-})
+});
