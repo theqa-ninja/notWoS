61aec20135004fb2004934a70c952d610dd7ff94
Amy Dang <kirontoo@gmail.com>
2022-03-27 15:26:23 -0700

    Populate valid guesses

--- frontend/src/components/Layouts/LayoutGame.tsx
+++ frontend/src/components/Layouts/LayoutGame.tsx
@@ -1,25 +1,27 @@
 import Sidebar from 'components/Sidebar/Sidebar';
 import WosBoard from 'components/WosBoard/WosBoard';
+import WosGuess from 'components/WosGuess';
+import { words } from 'utils/MockData';
 
 const LayoutGame = () => {
+  const list = words.sort();
   return (
     <>
       <main className="game">
         <section className="game-wos">
           <WosBoard annoucement="lunar_marya found a word" />
-          <div className="flex gap-4 justify-end items-start w-full h-full">
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-2 justify-between p-2 w-fit bg-black-3 rounded-lg">
-                <div className="flex gap-2 items-center">
-                  <span className="text-yellow">hi</span>
-                  <span className="text-white">xhumming</span>
-                </div>
-                <div className="flex gap-2">
-                  <span className="w-8 h-8 bg-rosewater rounded"></span>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ul className="wos-gameboard">
+            {list.map((i: string) => (
+              <WosGuess
+                key={i}
+                word={i}
+                player="kironto"
+                locked={Math.floor(Math.random() * 1) === 1}
+                hidden={Math.floor(Math.random() * 3) === 1}
+                length={i.length}
+              />
+            ))}
+          </ul>
         </section>
         <Sidebar />
       </main>
