354ccc1e5eb4008f3e4ba0b9f6d66ba156488643
Amy Dang <kirontoo@gmail.com>
2022-03-26 20:45:30 -0700

    Implement auto-scroll down

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -1,8 +1,22 @@
 import ChatMessage, { ChatMessageType } from 'components/Chat/ChatMessage';
+import { useEffect, useRef } from 'react';
 
 const Chat = ({ messages }: { messages: ChatMessageType[] }) => {
+  const chatRef = useRef<null | HTMLUListElement>(null);
+
+  // Auto-scroll down as messages are inserted
+  useEffect(() => {
+    if (chatRef.current !== null) {
+      chatRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+        inline: 'nearest'
+      });
+    }
+  }, [messages]);
+
   return (
-    <ul className="chat">
+    <ul className="chat" ref={chatRef}>
       {messages.map((m, i) => (
         <ChatMessage
           key={i.toString()}
