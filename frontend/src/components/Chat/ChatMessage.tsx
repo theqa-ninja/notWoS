e46a5bf24bdce5ff6bd933bda9947ed04527263e
Amy Dang <kirontoo@gmail.com>
2022-03-28 21:52:04 -0700

    Configure username color options

--- frontend/src/components/Chat/ChatMessage.tsx
+++ frontend/src/components/Chat/ChatMessage.tsx
@@ -7,7 +7,21 @@ export interface ChatMessageType {
 import classnames from 'classnames';
 
 function ChatMessage({ username, color, message }: ChatMessageType) {
-  const classes = classnames(`chat-username chat-username--${color}`);
+  const classes = classnames('chat-username', {
+    'chat-username--maroon': color === 'maroon',
+    'chat-username--red': color === 'red',
+    'chat-username--rosewater': color === 'rosewater',
+    'chat-username--flamingo': color === 'flamingo',
+    'chat-username--mauve': color === 'mauve',
+    'chat-username--pink': color === 'pink',
+    'chat-username--peach': color === 'peach',
+    'chat-username--yellow': color === 'yellow',
+    'chat-username--green': color === 'green',
+    'chat-username--teal': color === 'teal',
+    'chat-username--blue': color === 'blue',
+    'chat-username--sky': color === 'sky',
+    'chat-username--lavender': color === 'lavender'
+  });
   return (
     <li className="animate-fade-in chat-message">
       <span className={classes}>{username}</span>
