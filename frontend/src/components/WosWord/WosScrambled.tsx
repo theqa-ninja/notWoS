15a8c55ac8e196a728a3a0e4d0e6297935d59319
Amy Dang <kirontoo@gmail.com>
2022-04-02 16:06:06 -0700

    Setup to scramble ,un-hide hidden & fake letters

--- frontend/src/components/WosWord/WosScrambled.tsx
+++ frontend/src/components/WosWord/WosScrambled.tsx
@@ -2,86 +2,46 @@ import { ReactNode } from 'react';
 import WosBlock from 'components/WosWord/WosBlock';
 import { generateRandomLetter, scrambleWord } from 'utils/Scramble';
 import { useEffect, useState, useReducer } from 'react';
+import WosWord from 'utils/WosWord';
 
 interface WosScrambledProps {
-  word: string;
   lvl: Level;
 }
 
-/* TODO: maybe the fakes and hiddens should have a state too?
- * fake = { char: 'i', hidden: false, fake: true }
- */
-
-interface Anagram extends Level {
-  scrambled?: string[];
-}
-
-function WosScrambled({ word, lvl }: WosScrambledProps) {
-  // const numOfFakes = 1;
-  // const numOfHiddens = 2;
-  // const initState: Anagram = {
-  //   original: word,
-  //   scrambled: word.split(''),
-  //   fakes: [],
-  //   hiddens: []
-  // };
-
-  const [anagram, setAnagram] = useState<Anagram>(
-    Object.assign({ scrambled: [] }, lvl)
-  );
+// TODO: might be better to turn WosWord into a ContextProvider
+function WosScrambled({ lvl }: WosScrambledProps) {
+  const wosWord: WosWord = new WosWord(lvl);
+  const [anagram, setAnagram] = useState<Letter[]>([...wosWord.letters]);
 
   useEffect(() => {
     // TODO: only generate hiddens or fakes after a certain level
     // obfuscateWord();
-    const scrambled = anagram.letters.concat(anagram.fake_letters);
-    setAnagram((state: Anagram) => ({
-      ...state,
-      scrambled: scrambleWord(scrambled)
-    }));
   }, []);
 
   const [showHiddens, setShowHiddens] = useState<boolean>(false);
-
-  // function obfuscateWord() {
-  //   const temp: Anagram = anagram;
-
-  //   // NOTE: Order is important. A fake letter can also be hidden
-  //   for (let i = 0; i < numOfFakes; i++) {
-  //     const l = generateRandomLetter();
-  //     temp.fakes.push(l);
-  //     temp.scrambled.push(l);
-  //   }
-
-  //   for (let i = 0; i < numOfHiddens; i++) {
-  //     const randIndex = Math.floor(Math.random() * temp.scrambled.length);
-  //     temp.hiddens.push(temp.scrambled[randIndex]);
-  //   }
-
-  //   temp.scrambled = scrambleWord(temp.scrambled);
-  //   setAnagram((state: Anagram) => ({ ...state, ...temp }));
-  // }
-  //
-  console.log(anagram);
+  const [showFakes, setShowFakes] = useState<boolean>(false);
 
   function scramble() {
-    const scrambled: string[] = scrambleWord(anagram.scrambled ?? []);
-    setAnagram((state: Anagram) => {
-      return {
-        ...state,
-        scrambled: scrambled
-      };
-    });
+    setAnagram([...wosWord.scramble()]);
   }
 
-  // TODO: what if there are duplicate letters? All duplicates will show as hidden or fake
+  function unHideHiddens() {
+    wosWord.unHide();
+    setShowHiddens(true);
+  }
+
+  function unHideFakes() {
+    setShowFakes(true);
+  }
+
   return (
     <div className="wos-word" onClick={scramble}>
-      {anagram.scrambled?.map((l: string, i: number) => (
+      {anagram.map((l: Letter, i: number) => (
         <WosBlock
-          key={l + i}
-          letter={l}
-          hidden={anagram.hidden_letters.includes(l) && !showHiddens}
-          fake={anagram.fake_letters.includes(l)}
+          key={l.char + i}
+          letter={l.char}
+          hidden={l.hidden && !showHiddens}
+          fake={l.fake && showFakes}
         />
       ))}
     </div>
--- frontend/src/utils/WosWord.ts
+++ frontend/src/utils/WosWord.ts
@@ -1,22 +1,64 @@
 export interface IWosWord {
-  original: string;
-  scrambled: string;
-  hiddens: string[];
-  fakes: string[];
+  letters: Letter[];
 }
 
 export default class WosWord implements IWosWord {
-  public original = '';
-  public scrambled = '';
-  public hiddens: string[] = [];
-  public fakes: string[] = [];
+  public letters: Letter[] = [];
+  public showFakes = false;
 
-  constructor(word: string) {
-    this.original = word;
+  constructor(lvl: Level) {
+    // convert word into Type Letter
+    let markedHidden: {
+      [key: string]: boolean;
+    } = {};
+    const scramble: string[] = lvl.letters.concat(lvl.fake_letters);
+    this.letters = scramble.map((w: string): Letter => {
+      let hidden = false;
+
+      // TODO: might have to the same with fake letters
+      // check if it's a hidden letter and not already marked as hidden
+      // A word can have duplicate letters, only one should be marked hidden
+      if (lvl.hidden_letters.includes(w) && !(w in markedHidden)) {
+        markedHidden = Object.assign({ [w]: true }, markedHidden);
+        hidden = true;
+      }
+
+      return {
+        char: w,
+        hidden: hidden,
+        fake: lvl.fake_letters.includes(w)
+      };
+    });
+
+    this.scramble();
   }
 
-  // get scrambled() {
-  //   return this.scrambled.split('');
-  // }
-  // TODO: port word scrambling into here
+  get word(): string {
+    return this.letters
+      .map((l) => {
+        return l.char;
+      })
+      .join('');
+  }
+
+  public scramble(): Letter[] {
+    const cpy: Letter[] = this.letters;
+    for (let i = 0; i < this.letters.length; i++) {
+      const swapIndex = Math.floor(Math.random() * this.letters.length);
+      const tmp = cpy[i];
+      cpy[i] = cpy[swapIndex];
+      cpy[swapIndex] = tmp;
+    }
+
+    this.letters = cpy;
+    return cpy;
+  }
+
+  public unHide(): void {
+    this.letters.forEach((l: Letter) => (l.hidden = false));
+  }
+
+  public toggleFakes() {
+    this.showFakes = true;
+  }
 }
