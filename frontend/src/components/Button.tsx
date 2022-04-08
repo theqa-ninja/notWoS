502c411deb52e1e0b0174a7bb72fefbfebc8e274
Amy Dang <kirontoo@gmail.com>
2022-04-08 16:38:59 -0700

    Fix text, border & background color not applyin

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -17,26 +17,46 @@ function Button({
   popup = false,
   color = 'teal',
   kind = 'solid',
+  className,
   onClick,
   children,
   ...rest
 }: ButtonProps): JSX.Element {
-  const btnTextClasses = classnames('btn', {
-    'bg-maroon': color === Colors.maroon,
-    'bg-red': color === Colors.red,
-    'bg-rosewater': color === Colors.rosewater,
-    'bg-flamingo': color === Colors.flamingo,
-    'bg-mauve': color === Colors.mauve,
-    'bg-pink': color === Colors.pink,
-    'bg-peach': color === Colors.peach,
-    'bg-yellow': color === Colors.yellow,
-    'bg-green': color === Colors.green,
-    'bg-teal': color === Colors.teal,
-    'bg-blue': color === Colors.blue,
-    'bg-sky': color === Colors.sky,
-    'bg-lavender': color === Colors.lavender,
-    'btn-outline': kind === 'outline'
-  });
+  const btnTextClasses = classnames(
+    [
+      `btn ${kind === 'outline' && 'btn-outline'}`,
+      `${kind === 'outline' && `border-${color}`}`,
+      className
+    ].join(' '),
+    {
+      'bg-maroon': color === Colors.maroon,
+      'bg-red': color === Colors.red,
+      'bg-rosewater': color === Colors.rosewater,
+      'bg-flamingo': color === Colors.flamingo,
+      'bg-mauve': color === Colors.mauve,
+      'bg-pink': color === Colors.pink,
+      'bg-peach': color === Colors.peach,
+      'bg-yellow': color === Colors.yellow,
+      'bg-green': color === Colors.green,
+      'bg-teal': color === Colors.teal,
+      'bg-blue': color === Colors.blue,
+      'bg-sky': color === Colors.sky,
+      'bg-lavender': color === Colors.lavender,
+      '!text-maroon': kind === 'outline' && color === Colors.maroon,
+      '!text-red': kind === 'outline' && color === Colors.red,
+      '!text-rosewater': kind === 'outline' && color === Colors.rosewater,
+      '!text-flamingo': kind === 'outline' && color === Colors.flamingo,
+      '!text-mauve': kind === 'outline' && color === Colors.mauve,
+      '!text-pink': kind === 'outline' && color === Colors.pink,
+      '!text-peach': kind === 'outline' && color === Colors.peach,
+      '!text-yellow': kind === 'outline' && color === Colors.yellow,
+      '!text-green': kind === 'outline' && color === Colors.green,
+      '!text-teal': kind === 'outline' && color === Colors.teal,
+      '!text-blue': kind === 'outline' && color === Colors.blue,
+      '!text-sky': kind === 'outline' && color === Colors.sky,
+      '!text-lavender': kind === 'outline' && color === Colors.lavender
+    }
+  );
 
   const btnShadowClasses = classnames('btn-shadow', {
     'border-maroon': color === Colors.maroon,
