1f96057b5f402309356ced283a66c2966646a9a6
Amy Dang <kirontoo@gmail.com>
2022-04-10 19:20:29 -0700

    Add route to join room page

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import Loading from 'components/Loading';
+import JoinRoom from 'pages/JoinRoom';
 
 const Home = lazy(() => import('pages/Home'));
 const NotFound = lazy(() => import('pages/404'));
@@ -13,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/wos" element={<WOS />} />
+          <Route path="/wos/join" element={<JoinRoom />}>
+            <Route path=":id" element={<JoinRoom />} />
+          </Route>
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Suspense>
---
+++ frontend/src/pages/JoinRoom.tsx
@@ -0,0 +1,43 @@
+import { FormEvent, ChangeEvent, useState } from 'react';
+import Button from 'components/Button';
+import { useParams } from 'react-router-dom';
+interface JoinRoomProps {}
+
+function JoinRoom() {
+  const params = useParams();
+  const [roomCode, setRoomCode] = useState<string>(params.id ?? '');
+  const setCode = (event: ChangeEvent<HTMLInputElement>) => {
+    setRoomCode(event.target.value);
+  };
+
+  const submitCode = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`submit ${roomCode}`);
+  };
+  return (
+    <main className="flex flex-col grow justify-center items-center">
+      <div className="dialog">
+        <h3 className="dialog-title">Join Room</h3>
+        <form
+          className="flex flex-col gap-2 w-full items-center"
+          onSubmit={submitCode}
+        >
+          <label htmlFor="join-room">Enter your room code</label>
+          <input
+            id="join-room"
+            type="text"
+            value={roomCode}
+            onChange={setCode}
+            className="font-roboto"
+          />
+
+          <Button size="lg" popup color="green" className="mt-2">
+            join room
+          </Button>
+        </form>
+      </div>
+    </main>
+  );
+}
+
+export default JoinRoom;
