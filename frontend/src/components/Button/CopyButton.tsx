65fb7faa76564ecdc5eae0e6ae5f3f8d6d395ba6
Amy Dang <kirontoo@gmail.com>
2022-04-14 15:28:53 -0700

    Implement copy to clipboard button

--- frontend/src/components/Button/CopyButton.tsx
+++ frontend/src/components/Button/CopyButton.tsx
@@ -1,9 +1,22 @@
 import Button from './Button';
 import { ClipboardCopyIcon } from '@heroicons/react/solid';
+import { CopyButtonProps } from 'types/Button';
 
-function CopyButton({ ...rest }) {
+function CopyButton({ text, onFailure, onSuccess, ...rest }: CopyButtonProps) {
+  function copy() {
+    navigator.clipboard.writeText(text).then(
+      function () {
+        // apply on successful copy
+        onSuccess && onSuccess();
+      },
+      function () {
+        // apply on failure to copy
+        onFailure && onFailure();
+      }
+    );
+  }
   return (
-    <Button {...rest}>
+    <Button {...rest} iconOnly onClick={copy}>
       <ClipboardCopyIcon className="w-6 h-6" />
     </Button>
   );
