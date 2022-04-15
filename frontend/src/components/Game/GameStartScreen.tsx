32f9ffffac7313e59f0689cd43ffecd22c1e62ed
Amy Dang <kirontoo@gmail.com>
2022-04-15 15:28:26 -0700

    Should show the same code from route params

--- frontend/src/components/Game/GameStartScreen.tsx
+++ frontend/src/components/Game/GameStartScreen.tsx
@@ -1,20 +1,25 @@
 import Button from 'components/Button/Button';
 import CopyButton from 'components/Button/CopyButton';
 import { useGame } from 'context/GameProvider';
+import { useParams } from 'react-router-dom';
 
 const GameStartScreen = () => {
   const { advanceScreen } = useGame();
+  const params = useParams();
+  // TODO: if owner, show start game, else show waiting screen
   return (
     <>
       <main className="flex grow justify-center items-center w-full h-full">
         <section className="dialog">
           <h3 className="text-center heading-04">Copy to share:</h3>
           <div className="flex gap-4 justify-center">
-            <span className="w-fit text-5xl text-center uppercase">4jbd9</span>
+            <span className="w-fit text-5xl text-center uppercase">
+              {params.code}
+            </span>
             <CopyButton
               color="pink"
               popup
-              text={`http://${window.location.host}/wnos/join/4jbd9`}
+              text={`http://${window.location.host}/wnos/join/${params.code}`}
             />
           </div>
           <Button
