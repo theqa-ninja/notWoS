5281e77c38855b6fbf40dc0dc2dab1901e95561f
Amy Dang <kirontoo@gmail.com>
2022-05-06 12:33:59 -0700

    Disable random guseses

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -4,12 +4,15 @@ import { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import { Usernames } from 'utils/MockData';
 import ChatMessages from './ChatMessages';
 import Colors from 'utils/Colors';
+import { observer } from 'mobx-react';
+import { useUser } from 'context/UserProvider';
 
 function Chat() {
   const [messages, setMessages] = useState<ChatMessageType[]>([]);
   const [guess, setGuess] = useState<string>('');
 
   const colors: string[] = Object.keys(Colors);
+  const user = useUser();
 
   useEffect(() => {
     // makes sure the max amount of messages is always 200
@@ -22,25 +25,39 @@ function Chat() {
 
   const submitMessage = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    let message: string = Math.random()
-      .toString(24)
-      .substr(2, Math.random() * 10 + 2);
-    if (guess !== '') {
-      message = guess;
+    // let message: string = Math.random()
+    //   .toString(24)
+    //   .substr(2, Math.random() * 10 + 2);
+    // if (guess !== '') {
+    //   message = guess;
+    //   setGuess('');
+    // }
+    // const username: string =
+    //   Usernames[Math.floor(Math.random() * Usernames.length)];
+    // const color: string = colors[Math.floor(Math.random() * colors.length)];
+    // const newMessage: ChatMessageType = {
+    //   message,
+    //   username,
+    //   color: color as Color
+    // };
+
+    if (guess != '') {
+      const newMessage: ChatMessageType = {
+        message: guess,
+        username: user.user.username,
+        color: (user.user.guest ? getRandColor() : user.color) as Color
+      };
+      setMessages((s) => {
+        return [...s, newMessage];
+      });
+
       setGuess('');
     }
-    const username: string =
-      Usernames[Math.floor(Math.random() * Usernames.length)];
-    const color: string = colors[Math.floor(Math.random() * colors.length)];
-    const newMessage: ChatMessageType = {
-      message,
-      username,
-      color: color as Color
-    };
-    setMessages((s) => {
-      return [...s, newMessage];
-    });
   };
+
+  function getRandColor(): string {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
 
   function onGuess(e: ChangeEvent<HTMLInputElement>) {
     setGuess(e.target.value);
