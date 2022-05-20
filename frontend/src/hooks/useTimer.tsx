bda04f1025e770888ba817a0504bb488916918d5
Amy Dang <kirontoo@gmail.com>
2022-05-20 16:55:57 -0700

    Add state to know if timer has started or ended

--- frontend/src/hooks/useTimer.tsx
+++ frontend/src/hooks/useTimer.tsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from 'react';
 
-function useTimer(startTimer = false, duration = 30) {
+function useTimer(start = false, duration = 30) {
   const [count, setCount] = useState(duration);
   const [timerEnded, setTimerEnded] = useState<boolean>(false);
+  const [timerStarted, setTimerStarted] = useState<boolean>(start);
 
   useEffect(() => {
     if (count <= 0) {
+      setTimerStarted(false);
       setTimerEnded(true);
       return;
     }
 
-    if (startTimer) {
+    if (timerStarted) {
       const countdown = setInterval(() => {
         setCount((c) => c - 1);
       }, 1000);
 
       return () => clearInterval(countdown);
     }
-  }, [startTimer]);
+  }, [count, timerStarted]);
 
-  return { count, timerEnded };
+  function startTimer() {
+    setTimerStarted(true);
+  }
+
+  return { count, timerStarted, timerEnded, startTimer };
 }
 
 export default useTimer;
