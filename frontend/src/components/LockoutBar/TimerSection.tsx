38d08dbe7eec5d05a1467285ec6810c7b4128a08
Amy Dang <kirontoo@gmail.com>
2022-05-20 16:57:32 -0700

    Keep track of the timer and progress the bar

--- frontend/src/components/LockoutBar/TimerSection.tsx
+++ frontend/src/components/LockoutBar/TimerSection.tsx
@@ -5,26 +5,49 @@ import {
 } from '@heroicons/react/solid';
 
 import { useState, useEffect } from 'react';
+import useTimer from 'hooks/useTimer';
 
 interface TimerSectionProps {
-  locked: boolean;
-  countdown: number;
+  locked?: boolean;
+  duration?: number;
+  started?: boolean;
+  end?: boolean;
 }
 
-function TimerSection({ locked = true, countdown = 30 }: TimerSectionProps) {
-  // TODO: timer countdown and animate progress bar
-
+function TimerSection({
+  locked = true,
+  duration = 30,
+  started = false,
+  end = false
+}: TimerSectionProps) {
+  //TODO: NEEDS A WAY TO TELL PARENT COMPONENT THAT THE TIMER ENDED
   const [progress, setProgress] = useState<number>(100);
+  const timer = useTimer(started, duration);
+  const [lockedTimer, setLockedTimer] = useState<boolean>(locked);
 
   function calcProgress(currTime: number) {
-    return Math.floor((currTime / countdown) * 100);
+    return Math.floor((currTime / duration) * 100);
   }
+
+  useEffect(() => {
+    if (started) {
+      timer.startTimer();
+    }
+  }, [started]);
+
+  useEffect(() => {
+    // set the timer progress
+    if (timer.count > 0) {
+      setProgress(calcProgress(timer.count));
+    } else {
+      // timer done, unlock timer
+      setProgress(0);
+      setLockedTimer(false);
+    }
+  }, [timer.count]);
 
   return (
     <>
-      <div className="lockout-clock">
-        <ClockIcon className="text-yellow" />
-      </div>
       <div className="timer">
         <div className="timer-bar">
           <div
@@ -33,13 +56,15 @@ function TimerSection({ locked = true, countdown = 30 }: TimerSectionProps) {
           ></div>
         </div>
       </div>
-      <div className="timer-checkpoint">
-        {locked ? (
-          <LockClosedIcon className="timer-lock"></LockClosedIcon>
-        ) : (
-          <LockOpenIcon className="timer-lock"></LockOpenIcon>
-        )}
-      </div>
+      {!end ? (
+        <div className="timer-checkpoint">
+          {lockedTimer ? (
+            <LockClosedIcon className="timer-lock"></LockClosedIcon>
+          ) : (
+            <LockOpenIcon className="timer-lock"></LockOpenIcon>
+          )}
+        </div>
+      ) : null}
     </>
   );
 }
